e32b1b5a02e8b5194cfc6d3abf24246c5430f06c
corbincargil <corbin.cargil@gmail.com>
2022-04-14 17:53:34 -0500

    finished the calculator exercise.

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,26 +1,44 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+  return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(myArray) {
+  let sum = 0;
+  myArray.forEach(number => {
+      sum += number;
+  });
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(myArray) {
+  let product = 1;
+  myArray.forEach(number => {
+    product *= number;
+  });
+  return product;
 };
 
-const power = function() {
-	
+const power = function(base, exponent) {
+	return Math.pow(base,exponent);
 };
 
-const factorial = function() {
-	
+const factorial = function(number) {
+  let result = 1;
+  if (number === 0) {
+      
+  } else {
+    while (number > 1) {
+        result *= number
+        number--;
+    };
+  }
+  return result;
 };
+
 
 // Do not edit below this line
 module.exports = {
