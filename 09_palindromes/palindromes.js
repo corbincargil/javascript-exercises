b7f50ce07a1ab5506a3afed9ffa8cafefdd0396e
corbincargil <corbin.cargil@gmail.com>
2022-04-15 12:40:39 -0500

    finished the palindromes exercise (#09).

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,20 @@
-const palindromes = function () {
+const palindromes = function (myString) {
+    //Getting base string
+    myString = myString.toLowerCase().trim();
+    let base = myString.replace(/[^a-z]/g,"");
 
+    //checking if the base string is a palindrome
+    let forwardString = base;
+    let forwardArray = base.split("");
+    let reverseArray = forwardArray.reverse();
+    let reverseString = reverseArray.join("");
+
+    if (forwardString == reverseString){
+       return true; 
+    } else {
+        return false;
+    }
+        
 };
 
 // Do not edit below this line
