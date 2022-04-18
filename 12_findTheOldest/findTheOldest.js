bcb679d277d488de54ecd23a84204cb7a990ef9e
corbincargil <corbin.cargil@gmail.com>
2022-04-18 11:24:21 -0500

    finsihed the findTheOldest exercise (#12)

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,17 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(array) {
+    const oldest = array.sort((firstPerson,secondPerson) => {
+        if (!firstPerson.yearOfDeath) {
+            firstPerson.yearOfDeath = new Date().getFullYear()
+        }
+        if (!secondPerson.yearOfDeath) {
+            secondPerson.yearOfDeath = new Date().getFullYear()
+        }
+        firstPersonLived = firstPerson.yearOfDeath - firstPerson.yearOfBirth;
+        secondPersonLived = secondPerson.yearOfDeath - secondPerson.yearOfBirth;
+        return secondPersonLived - firstPersonLived;
+    })
+    console.log(oldest[0]);
+    return oldest[0];
 };
 
 // Do not edit below this line
