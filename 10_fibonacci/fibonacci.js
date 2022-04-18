461018f0118b679a98677d9183318ac050dc466d
corbincargil <corbin.cargil@gmail.com>
2022-04-18 09:09:13 -0500

    finished the fibonacci exercise (#10)

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    if (typeof num === 'string') { num = Number(num);}
+    if (num == 0) {return 0;} 
+    else if (num <= 2 && num >0) {return 1;} 
+    else if (num > 2) {
+        fibArray = [0,1];
+        let nextNum;
+        for (let i = 2; i < num+1; i++) {
+            nextNum = fibArray[i-1] + fibArray[i-2];
+            fibArray.push(nextNum);  
+        }
+        return(fibArray[num]);
+    } else {
+        return "OOPS";
+    }
 };
 
 // Do not edit below this line
